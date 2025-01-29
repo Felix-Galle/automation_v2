141d779ad191e4fd29e5418ffca5476565080e3c
Felix-Galle <felixjohngalle@gmail.com>
2025-01-29 08:59:52 +0100

    sync

--- logger.py
+++ logger.py
@@ -2,22 +2,28 @@ import sys
 import os
 from datetime import datetime
 
-LOG_DIR = 'log/pc_info-logs'
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR)
+PC_LOG_DIR = 'log/pc_info-logs'
+ERROR_LOG_DIR = 'log/errors-logs'
+GENERAL_LOG_DIR = 'log/general-logs'
 
-def log_message(message):
+if not os.path.exists(PC_LOG_DIR):
+    os.makedirs(PC_LOG_DIR)
+if not os.path.exists(ERROR_LOG_DIR):
+    os.makedirs(ERROR_LOG_DIR)
+if not os.path.exists(GENERAL_LOG_DIR):
+    os.makedirs(GENERAL_LOG_DIR)
+
+def general_log(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log_file_path = os.path.join(LOG_DIR, f"general_log.txt")
+    log_file_path = os.path.join(PC_LOG_DIR, f"general_log-{datetime.now()}.txt")
     with open(log_file_path, 'a') as log_file:
         log_file.write(f"{timestamp} - {message}\n")
 
-def log_error(error_message):
+def error_log(error_message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log_file_path = os.path.join(LOG_DIR, f"error_log.txt")
+    log_file_path = os.path.join(ERROR_LOG_DIR, f"error_log-{datetime.now()}.txt")
     with open(log_file_path, 'a') as log_file:
         log_file.write(f"{timestamp} - {error_message}\n")
 
-if __name__ == "__main__":
-    message = sys.argv[1]
-    log_message(message)
+
+
